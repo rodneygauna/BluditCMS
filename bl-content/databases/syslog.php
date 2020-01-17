7aec9a36abd7fe6d8d29fe5ba75492b988e5956f
Rodney Gauna <rodneygauna@gmail.com>
2020-01-16 16:54:50 -0800

    Install and created Bludit

---
+++ bl-content/databases/syslog.php
@@ -0,0 +1,11 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+[
+    {
+        "date": "2020-01-16 16:53:56",
+        "dictionaryKey": "welcome-to-bludit",
+        "notes": "",
+        "idExecution": "5e2105a48cf4c",
+        "method": "POST",
+        "username": "admin"
+    }
+]
---
+++ bl-content/databases/users.php
@@ -0,0 +1,23 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{
+    "admin": {
+        "nickname": "Admin",
+        "firstName": "Administrator",
+        "lastName": "",
+        "role": "admin",
+        "password": "509ec694709f832af2653e1a3094b5da8442d2f2",
+        "salt": "5e2105a48ce65",
+        "email": "",
+        "registered": "2020-01-16 16:53:56",
+        "tokenRemember": "",
+        "tokenAuth": "b8bd4ca945f13de07992f641dc50bed4",
+        "tokenAuthTTL": "2009-03-15 14:00",
+        "twitter": "",
+        "facebook": "",
+        "instagram": "",
+        "codepen": "",
+        "linkedin": "",
+        "github": "",
+        "gitlab": ""
+    }
+}
